0ed59c771bb9e48313c7d458551b381a408336f7
Truong <hoangtruong.uit@gmail.com>
2017-12-09 14:55:47 +0700

    update config file

--- wp-config.php
+++ wp-config.php
@@ -17,43 +17,34 @@
  *
  * @package WordPress
  */
-
 // ** MySQL settings - You can get this info from your web host ** //
+$url = parse_url(getenv('DATABASE_URL') ? getenv('DATABASE_URL') : getenv('CLEARDB_DATABASE_URL'));
+
 /** The name of the database for WordPress */
-define('DB_NAME', 'my-blog');
+define('DB_NAME', trim($url['path'], '/'));
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', $url['user']);
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', $url['pass']);
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', $url['host']);
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8mb4');
+define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
-
-/**#@+
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
-define('AUTH_KEY',         'EeW%1ApI!![fN*eR401QXmsu85Max#}_y2GTw:0mD%ceT>l9K}U+fC}y|:k8avLi');
-define('SECURE_AUTH_KEY',  'Kot6<$v6!o.4SXad`6._p:G!e3G&_wd/H;U:Yrv_Y4wNmJ]/gPYlKW[87R@ksg.3');
-define('LOGGED_IN_KEY',    '|_yvr[|wuBC>5)_4Z}Clt($}WZ.Vo<9vcF$#Y*dX{P|*.s+2|y/sO@OV3k6ZG)n+');
-define('NONCE_KEY',        ' d0VLW?<W<y;sQmLwJZXrKQ4W[5HW;y[=]0f6S{cqWJrlz{#f.wGp,R.$.x?SM3t');
-define('AUTH_SALT',        ',1Ng.zQI8KdddJisP+zvc`FMk?>^L!DscGoxfqF]B,C!sA_!ie.>b|SZMOu%z:y/');
-define('SECURE_AUTH_SALT', '^TF1,+ZL4:v_$h00d>|Z)[&~i2.prNg*/aHv>3`Q!/p$3o8UrIS]<fgBt+X2@5@e');
-define('LOGGED_IN_SALT',   '`:Fdi%f*{pAf^,f<1%)C[a)C)k}l7Vv<(tU2Ye@U=mM,NIjQO`,mh!nn%Rc=XgD&');
-define('NONCE_SALT',       'tP_MjGnX!gR/z)B1vTPC@x0;}zSnJu MhsF3tz^KIi4S,,VsoPq6,+L}]gody-Ts');
+define('AUTH_KEY',         getenv('AUTH_KEY'));
+define('SECURE_AUTH_KEY',  getenv('SECURE_AUTH_KEY'));
+define('LOGGED_IN_KEY',    getenv('LOGGED_IN_KEY'));
+define('NONCE_KEY',        getenv('NONCE_KEY'));
+define('AUTH_SALT',        getenv('AUTH_SALT'));
+define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
+define('LOGGED_IN_SALT',   getenv('LOGGED_IN_SALT'));
+define('NONCE_SALT',       getenv('NONCE_SALT'));
 
 /**#@-*/
 
